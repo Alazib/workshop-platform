899dc7c99b1598a7c73cf3feeec9efed5cae85ae
J.Alberto <alazib.developer@gmail.com>
2025-12-04 19:32:15 +0100

    feat(aplication/use case): Slice1. Define execute method on RegisterToSessionUseCase

--- backend/application/use_cases/register_to_session.py
+++ backend/application/use_cases/register_to_session.py
@@ -1,13 +1,30 @@
 from domain.ports.registration_repository import RegistrationRepository
 from domain.ports.session_repository import SessionRepository
+from domain.entities.registration import Registration, RegistrationStatus
 
 
 class RegisterToSessionUseCase:
     def __init__(
         self, session_repo: SessionRepository, registration_repo: RegistrationRepository
     ) -> None:
-
         self.session_repo = session_repo
         self.registration_repo = registration_repo
 
-    def execute(self, session_id: int, user_id: int) -> None: ...
+    def execute(self, session_id: int, user_id: int) -> Registration:
+        session = self.session_repo.get_session_by_id(session_id)
+        if session is None:
+            raise ValueError("Sesión no encontrada")
+
+        if not session.can_accept_registrations():
+            raise ValueError("La sesión no acepta inscripciones")
+
+        registration = Registration(
+            id=0,
+            session_id=session.id,
+            user_id=user_id,
+            status=RegistrationStatus.PENDING,
+        )
+
+        self.registration_repo.save_registration(registration)
+
+        return registration
