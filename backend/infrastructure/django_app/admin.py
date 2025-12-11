bdca3a7ecaa0e599278b7f34668bbe81837b0d24
J.Alberto <alazib.developer@gmail.com>
2025-12-11 17:24:52 +0100

    feat(infrastructure): Slice1. Create admin dashboard [#3]

--- backend/infrastructure/django_app/admin.py
+++ backend/infrastructure/django_app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import SessionModel, RegistrationModel
 
-# Register your models here.
+
+@admin.register(SessionModel)
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "status", "starts_at", "ends_at", "capacity_max")
+    list_filter = ("status", "starts_at")
+    search_fields = ("title",)
+
+
+@admin.register(RegistrationModel)
+class RegistrationAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "session_id",
+        "user_id",
+        "status",
+        "created_at",
+        "confirmation_date",
+    )
+    list_filter = ("status", "created_at")
+    search_fields = ("user_id",)
