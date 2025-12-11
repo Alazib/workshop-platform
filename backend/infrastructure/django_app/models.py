25bf9a89663c5980c8e5d3a6f569bddded384347
J.Alberto <alazib.developer@gmail.com>
2025-12-11 17:24:11 +0100

    feat(infrastructure): Slice1. Add RegistrationModel in Django ORM [#3]

--- backend/infrastructure/django_app/models.py
+++ backend/infrastructure/django_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class SessionModel(models.Model):
 
-    # Se opta por no declarar el "id" de SessionModel pues Django lo añade automáticamente:
+    # Se opta por no declarar los "id" en los modelos pues Django lo añade automáticamente:
     # id = AutoField(primary_key=True)
 
     workshop_id = models.IntegerField()
@@ -40,5 +40,50 @@ class SessionModel(models.Model):
         db_table = "sessions"
 
     def __str__(self) -> str:
-        return f"{self.title} ({self.get_status_display()})"  # type: ignore -> (se ignora error pues 'get_status_display()' lo añade Django en runtime para campos con CHOICES y
+        return f"La sesión {self.title} se encuentra en estado {self.get_status_display()}"  # type: ignore -> (se ignora error pues 'get_status_display()' lo añade Django en runtime para campos con CHOICES y
         # el analizador de tipos está mirando tan solo el código estático).
+
+
+class RegistrationModel(models.Model):
+
+    # Relación con SessionModel (clave foránea en Django)
+    session_id = models.ForeignKey(
+        "django_app.SessionModel",
+        on_delete=models.CASCADE,
+        related_name="registrations",
+    )
+
+    user_id = models.IntegerField()
+
+    STATUS_RESERVED = "reserved"
+    STATUS_PENDING = "pending"
+    STATUS_CONFIRMED = "confirmed"
+    STATUS_CANCELLED_BY_USER = "cancelled_by_user"
+    STATUS_CANCELLED_BY_ORGANIZER = "cancelled_by_organizer"
+    STATUS_ATTENDED = "attended"
+    STATUS_NO_SHOW = "no_show"
+
+    STATUS_CHOICES = [
+        (STATUS_RESERVED, "Reserved"),
+        (STATUS_PENDING, "Pending"),
+        (STATUS_CONFIRMED, "Confirmed"),
+        (STATUS_CANCELLED_BY_USER, "Cancelled by user"),
+        (STATUS_CANCELLED_BY_ORGANIZER, "Cancelled by organizer"),
+        (STATUS_ATTENDED, "Attended"),
+        (STATUS_NO_SHOW, "No show"),
+    ]
+
+    status = models.CharField(
+        max_length=30,
+        choices=STATUS_CHOICES,
+        default=STATUS_PENDING,
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    confirmation_date = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        db_table = "registrations"
+
+    def __str__(self) -> str:
+        return f"El registro del usuario {self.user_id} para la sesión {self.session_id} se encuentra en estado ({self.get_status_display()})"  # type: ignore
