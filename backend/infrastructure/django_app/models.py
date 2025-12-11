c64a5163065b196be7a8901255e4f0e6e2fbe6ac
J.Alberto <alazib.developer@gmail.com>
2025-12-11 15:55:23 +0100

    feat(infrastructure): Slice1. Add SessionModel in Django ORM [#3]

--- backend/infrastructure/django_app/models.py
+++ backend/infrastructure/django_app/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class SessionModel(models.Model):
+
+    # Se opta por no declarar el "id" de SessionModel pues Django lo añade automáticamente:
+    # id = AutoField(primary_key=True)
+
+    workshop_id = models.IntegerField()
+    title = models.CharField(max_length=200)
+    starts_at = models.DateTimeField()
+    ends_at = models.DateTimeField()
+    capacity_max = models.IntegerField()
+
+    STATUS_DRAFT = "draft"
+    STATUS_ANNOUNCED = "announced"
+    STATUS_PUBLISHED = "published"
+    STATUS_FULL = "full"
+    STATUS_CANCELLED = "cancelled"
+    STATUS_COMPLETED = "completed"
+    STATUS_ARCHIVED = "archived"
+
+    STATUS_CHOICES = [
+        (STATUS_DRAFT, "Draft"),
+        (STATUS_ANNOUNCED, "Announced"),
+        (STATUS_PUBLISHED, "Published"),
+        (STATUS_FULL, "Full"),
+        (STATUS_CANCELLED, "Cancelled"),
+        (STATUS_COMPLETED, "Completed"),
+        (STATUS_ARCHIVED, "Archived"),
+    ]
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default=STATUS_DRAFT,
+    )
+
+    class Meta:
+        db_table = "sessions"
+
+    def __str__(self) -> str:
+        return f"{self.title} ({self.get_status_display()})"  # type: ignore -> (se ignora error pues 'get_status_display()' lo añade Django en runtime para campos con CHOICES y
+        # el analizador de tipos está mirando tan solo el código estático).
